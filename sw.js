974af280e60fbca2a1f9bad4836ac2f96d322363
tareqrayhan321 <tareqrayhan1@gmail.com>
2026-06-04 14:51:10 +0600

    Update cache name to version 4

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    PWA অফলাইন সাপোর্টের জন্য
 ══════════════════════════════════════════ */
 
-const CACHE_NAME = 'imran-cloth-store-v3';
+const CACHE_NAME = 'imran-cloth-store-v4';
 
 /* যেসব ফাইল ইনস্টলেশনের সময়ই ক্যাশ হবে */
 const PRECACHE_URLS = [
