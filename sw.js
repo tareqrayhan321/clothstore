d3bd523f970ea50dbbc76d327288f6cd0c6b5522
tareqrayhan321 <tareqrayhan1@gmail.com>
2026-07-07 00:00:09 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    PWA অফলাইন সাপোর্টের জন্য
 ══════════════════════════════════════════ */
 
-const CACHE_NAME = 'imran-cloth-store-v12';
+const CACHE_NAME = 'imran-cloth-store-v13';
 
 /* যেসব ফাইল ইনস্টলেশনের সময়ই ক্যাশ হবে */
 const PRECACHE_URLS = [
