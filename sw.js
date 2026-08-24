79c55d95ce324911f5787b718a607dbd1af371a2
tareqrayhan321 <tareqrayhan1@gmail.com>
2026-08-25 05:53:20 +0600

    Update CACHE_NAME to version v19

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    PWA অফলাইন সাপোর্টের জন্য
 ══════════════════════════════════════════ */
 
-const CACHE_NAME = 'imran-cloth-store-v18;
+const CACHE_NAME = 'imran-cloth-store-v19;
 
 /* যেসব ফাইল ইনস্টলেশনের সময়ই ক্যাশ হবে */
 const PRECACHE_URLS = [
